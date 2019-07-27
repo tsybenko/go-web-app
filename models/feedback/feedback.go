44775cdcd4f1994ffed2e2408ba9311770a8b440
tsybenko <tsybenko.dmitry@gmail.com>
2019-07-27 22:18:05 +0300

    Feedback model [+] added "Delete" method [+] changed GetAll method, added ordering by id field
    Contact
    [+] added bootstrap v4.3 to view
    [+] layout of view
    
    Misc
    [+] file formatting

--- models/feedback/feedback.go
+++ models/feedback/feedback.go
@@ -13,8 +13,9 @@ type Feedback struct {
 
 const table string = "feedback"
 
+// TODO realize field selection
 func GetAll(db *sql.DB) map[int]Feedback {
-	query := fmt.Sprintf("SELECT id, email, text FROM `%s`", table)
+	query := fmt.Sprintf("SELECT id, email, text FROM `%s` ORDER BY id DESC", table)
 	rows, err := db.Query(query)
 	if err != nil {
 		panic(err.Error())
@@ -40,4 +41,12 @@ func Create(db *sql.DB, feedback *Feedback) bool {
 		return true
 	}
 	return false
+}
+
+func Delete(db *sql.DB, id int) {
+	query := fmt.Sprintf("DELETE FROM %s WHERE id = %d", table, id)
+	_, err := db.Exec(query)
+	if err != nil {
+		panic(err.Error())
+	}
 }
